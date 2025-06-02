dd9c821b7df57b1f85928dadb6d7ea847f9a592b
orintarou <sharedaccount@prans-mbp.mynetworksettings.com>
2025-06-02 04:59:06 -0400

    switched to top-level await for minimal line by line readibility

--- flower_main.ts
+++ flower_main.ts
@@ -2,19 +2,18 @@ import * as readline from 'readline/promises';
 import {stdin as input, stdout as output} from 'process';
 import axios from 'axios';
 
-const rl = readline.createInterface({input, output});
-const APITOKEN = "<hidden>";
+const rl: any = readline.createInterface({input, output});
+const APITOKEN: string = "<hidden>";
 
-let getColor = async function(){
-    let color = await rl.question("\nEnter the color you'd like to check out!\n");    
-    rl.close();
-    return color.toLowerCase();
-} 
+let color = await rl.question("\nEnter the color you'd like to check out!\n");
 
-getColor().then((color: string) => {
-        axios.get("https://trefle.io/api/v1/species?filter%5Bflower_color%5D=" + color + "&token=" + APITOKEN).then((result) => {
-            let myPlants = result;
-            console.log(myPlants.data);
-        });
-    });
+rl.close();
 
+let myPlants = await axios.get("https://trefle.io/api/v1/species?filter%5Bflower_color%5D=" + color + "&token=" + APITOKEN);
+
+
+let myImageURLS = [];
+
+for(var i in myPlants.data.data){
+    myImageURLS.push(myPlants.data.data[i].image_url);
+}
